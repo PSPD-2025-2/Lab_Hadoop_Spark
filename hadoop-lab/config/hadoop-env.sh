5be7927c59e63356f18122c9a49fa750c8f8732d
Guilherme Westphall <guilhermewestphall@gmail.com>
2025-11-03 21:10:48 -0300

    fix conteiners

--- hadoop-lab/config/hadoop-env.sh
+++ hadoop-lab/config/hadoop-env.sh
@@ -1,1 +1,9 @@
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+export HADOOP_HOME=/home/hadoop/hadoop
+export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
+
+# estes 4 apontam para a MESMA dist (monolítica)
+export HADOOP_COMMON_HOME=$HADOOP_HOME
+export HADOOP_HDFS_HOME=$HADOOP_HOME
+export HADOOP_YARN_HOME=$HADOOP_HOME
+export HADOOP_MAPRED_HOME=$HADOOP_HOME
